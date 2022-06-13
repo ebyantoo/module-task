86148255231d6e29320853d49d20de639567668b
ebyantoo <erbiantonug@gmail.com>
2022-06-13 15:02:55 +0700

    add module task

---
+++ module.go
@@ -0,0 +1,32 @@
+package moduletask
+
+import "fmt"
+
+func CekGanjilGenap(bilangan ...int) int {
+	angka := 0
+	for _, i := range bilangan {
+		if i%2 == 0 {
+			fmt.Println(i, " Adalah Bilangan Genap")
+		} else {
+			fmt.Println(i, " Adalah Bilangan Ganjil")
+		}
+	}
+	return angka
+}
+
+func CekGanjilGenap2(angka int) int {
+	for bilangan := 1; bilangan <= angka; bilangan++ {
+		fmt.Print("Masukan Bilangan : ")
+		fmt.Scan(&bilangan)
+		if bilangan <= angka {
+			if bilangan%2 == 0 {
+				fmt.Println(bilangan, " Adalah Bilangan Genap")
+			} else {
+				fmt.Println(bilangan, " Adalah Bilangan Ganjil")
+			}
+		} else {
+			fmt.Println("Hanya Sampai ", angka)
+		}
+	}
+	return angka
+}
